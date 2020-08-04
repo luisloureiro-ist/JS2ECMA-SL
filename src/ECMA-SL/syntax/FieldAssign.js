5fd006ad8f5901bfd4c39843db0a45aad0162855
Luís Loureiro <luismaloureiro@tecnico.ulisboa.pt>
2020-08-04 17:06:32 +0100

    Expression inside square brackets must be a string.

--- src/ECMA-SL/syntax/FieldAssign.js
+++ src/ECMA-SL/syntax/FieldAssign.js
@@ -1,16 +1,16 @@
-const Stmt = require("./Stmt")
+const Stmt = require("./Stmt");
 
 class FieldAssign extends Stmt {
   constructor(expressionObject, expressionField, expressionValue) {
-    super()
-    this.expressionObject = expressionObject
-    this.expressionField = expressionField
-    this.expressionValue = expressionValue
+    super();
+    this.expressionObject = expressionObject;
+    this.expressionField = expressionField;
+    this.expressionValue = expressionValue;
   }
 
   toString() {
-    return `${this.expressionObject.toString()}[${this.expressionField.toString()}] := ${this.expressionValue.toString()}`
+    return `${this.expressionObject.toString()}["${this.expressionField.toString()}"] := ${this.expressionValue.toString()}`;
   }
 }
 
-module.exports = FieldAssign
+module.exports = FieldAssign;
